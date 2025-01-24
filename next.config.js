61b73c4f38453f070f0bf0058cc93959d16f3b9b
Jesus Salatiel <jesus.bustamante@globant.com>
2025-01-23 20:07:44 -0600

    fix(ci): stablish connections

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    output: 'export', // Exporta la aplicación como archivos estáticos
-    distDir: 'build', // Especifica la carpeta de salida como 'build'
-    trailingSlash: true, // Opcional: Añade una barra final a las URLs
-    images: {
-      unoptimized: true, // Desactiva la optimización de imágenes si es necesario
-    },
-    typescript: {
-      ignoreBuildErrors: false, // Cambia a true si quieres ignorar errores de TypeScript durante el build
-    },
-  };
-  
-  module.exports = nextConfig;
+  /* config options here */
+  output: "export",
+}
+ 
+module.exports = nextConfig
