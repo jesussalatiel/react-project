14a7cb1678549e278b2a67af327ae0c75d14ecb0
Jesus Salatiel <jesus.bustamante@globant.com>
2025-01-23 18:46:22 -0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+const nextConfig = {
+    output: 'export', // Exporta la aplicación como archivos estáticos
+    distDir: 'build', // Especifica la carpeta de salida como 'build'
+    trailingSlash: true, // Opcional: Añade una barra final a las URLs
+    images: {
+      unoptimized: true, // Desactiva la optimización de imágenes si es necesario
+    },
+    typescript: {
+      ignoreBuildErrors: false, // Cambia a true si quieres ignorar errores de TypeScript durante el build
+    },
+  };
+  
+  module.exports = nextConfig;
